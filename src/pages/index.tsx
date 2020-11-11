a4557b649ff8354bec0dd0cc0d84921f39735000
maanizfar <maanizfar@gmail.com>
2020-11-11 19:17:42 +0500

    frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import React, { useContext } from "react"
+import React, { useState } from "react"
 import { useQuery, useMutation } from "@apollo/client"
 import gql from "graphql-tag"
 import SEO from "../components/seo"
+
+interface Bookmark {
+  id: number
+  title: string
+  description: string
+  link: string
+}
+
+interface Bookmarks {
+  allBookmarks: Bookmark[]
+}
 
 const allBookmarksQuery = gql`
   {
@@ -14,8 +25,36 @@ const allBookmarksQuery = gql`
   }
 `
 
+const addBookmarkMutation = gql`
+  mutation addBookmark($title: String!, $description: String!, $link: String!) {
+    addBookmark(title: $title, description: $description, link: $link) {
+      id
+    }
+  }
+`
+
 const IndexPage = () => {
-  const { loading, data, error } = useQuery(allBookmarksQuery)
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [link, setLink] = useState("")
+
+  const { loading, data, error } = useQuery<Bookmarks>(allBookmarksQuery)
+  const [addBookmark] = useMutation(addBookmarkMutation)
+
+  const onSubmitForm = () => {
+    addBookmark({
+      variables: {
+        title,
+        description,
+        link,
+      },
+      refetchQueries: [{ query: allBookmarksQuery }],
+    }).then(() => {
+      setTitle("")
+      setDescription("")
+      setLink("")
+    })
+  }
 
   if (loading) return <p>Loading...</p>
 
@@ -25,8 +64,42 @@ const IndexPage = () => {
     <>
       <SEO title="Home" />
       <h1>Hi people</h1>
+
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          onSubmitForm()
+        }}
+      >
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
+        <label>Description</label>
+        <input
+          type="text"
+          name="desc"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          required
+        />
+        <label>Link</label>
+        <input
+          type="text"
+          name="link"
+          value={link}
+          onChange={e => setLink(e.target.value)}
+          required
+        />
+        <input type="submit" value="Add" />
+      </form>
+
       <ul>
-        {data.allBookmarks.map(bookmark => (
+        {data?.allBookmarks.map(bookmark => (
           <li key={bookmark.id}>
             <h4>{bookmark.title}</h4>
             <p>{bookmark.description}</p>
